3f17c71a7edf68832d44b9d9c96eff24dcd04384
lijackk <lijack@umich.edu>
2026-01-07 22:30:34 -0500

    Delete old (incorrect) pipeline

--- snakemake-pipeline/R/3_R_none.R
+++
@@ -1,8 +0,0 @@
-
-
-gwas_info <- snakemake@input[["gwas_info"]]
-cat(gwas_info, "\n")
-names <- readr::read_csv(gwas_info)$name
-n <- length(names)
-ret <- list(R = diag(n, nrow = n), names = names)
-saveRDS(ret, file = snakemake@output[["out"]])
